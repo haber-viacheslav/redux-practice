25080883e60d194744a27c684ae2e85dec9eebe2
Haber Viacheslav <gaberslavik@gmail.com>
2023-02-14 13:01:53 +0200

    added helpers

---
+++ src/components/helpers/getVisibleTasks.jsx
@@ -0,0 +1,12 @@
+import { statusFilters } from 'redux/constants';
+
+export const getVisibleTasks = (tasks, statusFilter) => {
+  switch (statusFilter) {
+    case statusFilters.active:
+      return tasks.filter(task => !task.completed);
+    case statusFilters.completed:
+      return tasks.filter(task => task.completed);
+    default:
+      return tasks;
+  }
+};
